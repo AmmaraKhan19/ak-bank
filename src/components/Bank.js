e0f9cf148db4847c3d7c335a92a4b133be13040c
Ammara Khan <ammarak281@gmail.com>
2023-05-04 15:02:48 +0500

    Add action creators and reducers

---
+++ src/components/Bank.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Bank = () => {
+     return (
+          <div>
+               <h5>Deposit/Withdraw Money</h5>
+               <button className="btn btn-primary mx-1">-</button>
+               Update Balance
+               <button className="btn btn-primary mx-1">+</button>
+          </div>
+     )
+}
+
+export default Bank
