ff8cf72b65cb62b4afc729a617a3adb2579358ff
Ammara Khan <ammarak281@gmail.com>
2023-05-04 13:39:16 +0500

    redux app first commit

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+     return (
+          <div>
+               <h5>Addidas Shoes - Rs. 100</h5>
+               <button className="btn btn-primary mx-1">-</button>               
+               Add to Cart
+               <button className="btn btn-primary mx-1">+</button>
+          </div>
+     )
+}
+
+export default Shop
